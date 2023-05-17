52efb34290d6b4d69fa85acd9a908faed5160da6
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-17 14:09:34 -0400

    "Commit 3 - I added list elements.

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -10,13 +10,24 @@ $(() => {
 
   const $h1 = $("<h1>").text("Hogwarts");
   console.log($h1);
+  $container.append($h1);
 
-  $container.append($h1); //appending h1 to container
+  const $h2 = $("<h2>").text("Taylor Murdock");
+  const $h3 = $("<h3>").text("Ravenclaw");
+  const $h4Pet = $("<h4>").text("Opal").addClass("pet toad");
+  const $h4Wand = $("<h4>").text("Elder Wand");
 
-  const $h2 = $("<h2>").text("Taylor Murdock"); //h2 element with your name
-  const $h3 = $("<h3>").text("Ravenclaw"); //h3 element with your house
-  const $h4Pet = $("<h4>").text("Opal").addClass("pet toad"); //pets name and class
+  $container.append($h2, $h3, $h4Pet, $h4Wand);
 
-  const $h4Wand = $("<h4>").text("Elder Wand"); //name of my wand
-  $container.append($h2, $h3, $h4Pet, $h4Wand); //appending these items to the container just like we did with h1
+  const $ul = $("<ul>").attr("storage", "trunk"); // Create unordered list with attribute "storage" and value "trunk"
+
+  const $li1 = $("<li>").text("Butter Beer");
+  const $li2 = $("<li>").text("Invisibility Cloak").addClass("secret"); // Add class "secret" to the list item
+  const $li3 = $("<li>").text("Magic Map").addClass("secret");
+  const $li4 = $("<li>").text("Time Turner").addClass("secret");
+  const $li5 = $("<li>").text("Leash").addClass("pet toad"); // Use the same class as your pet
+  const $li6 = $("<li>").text("Bertie Bott's Every Flavor [Jelly] Beans");
+
+  $ul.append($li1, $li2, $li3, $li4, $li5, $li6); // Append list items to the unordered list
+  $container.append($ul); // Append the unordered list to the container div
 });
