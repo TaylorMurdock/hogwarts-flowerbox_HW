37a21799d8ae4986127acd6280120203075ccf13
Taylor Murdock <tjmurd4545@gmail.com>
2023-05-17 13:53:41 -0400

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -3,3 +3,14 @@ if (typeof jQuery == "undefined") {
 } else {
   console.log("I did it! I linked jQuery and this js file!");
 }
+
+$(() => {
+  const $container = $("#container"); //Query for your div with the id of container and set it to a variable named $container
+  console.log($container);
+
+  const $h1 = $("<h1>"); //Create an <h1> element and set it to a variable called $h1
+  console.log($h1);
+
+  $h1.text("Hogwarts"); //Add some text inside the h1 element. Example text: 'Hogwarts'
+  $container.append($h1);
+});
